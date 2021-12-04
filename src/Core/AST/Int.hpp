6fd78ca41e33c290f1c2ee1f22180ba4f8b6c358
marcos <marcos30004347@gmail.com>
2021-12-03 21:12:32 -0300

    refactor bint methods

--- src/Core/AST/Int.hpp
+++ src/Core/AST/Int.hpp
@@ -12,6 +12,7 @@
 #include <iomanip>
 #include <iostream>
 #include <cassert>
+#include <cstring>
 
 #define pow2(e) (1 << e)
 
@@ -23,11 +24,12 @@
 
 template <char exp = 30, typename single_type = uint32_t, typename double_type = uint64_t>
 class bint {
-	static_assert(sizeof(double_type) >= 2*sizeof(single_type));
+	static_assert(sizeof(double_type) >= 2*sizeof(single_type),
+								"sizeof(double_type) needs to be at least twice"
+								"as big as sizeof(single_type)");
 
 	// digit2_t is a type capable of holding
 	// at least two elements of type single_type
-	using digit2_t = double_type;
 
 	static const single_type base = pow2(exp);
 	static const single_type mask = base - 1;
@@ -35,22 +37,58 @@ public:
 
 	// digit one is the type used to store every digit of the number base 2^exp
 	using digit_t = single_type;
-
+	using digit2_t = double_type;
+	using bint_t = bint<exp, digit_t, digit2_t>;
 
   digit_t *digit;
   size_t size;
+	short sign;
 
-  bint(digit_t *d, size_t s) : digit{d}, size{s} {}
-  ~bint() { delete[] digit; }
+  bint(digit_t *d, size_t s, short sign = 1) : digit{d}, size{s}, sign{sign} {}
+	bint() : digit{nullptr}, size{0}, sign{1} {}
+	~bint() { if(digit) delete[] digit; }
+
+	void resize(uint64_t s) {
+		size = s;
+
+		if(digit) delete digit;
+
+		if(s == 0) {
+			digit = nullptr;
+			return;
+		}
+
+		digit = new digit_t[s];
+	}
 
   void trim() {
-    while (!digit[size - 1])
-      size--;
-		digit = realloc(digit, sizeof(digit_t)*size);
+		if(size == 0) return;
+
+    size_t k = size - 1;
+
+    while (k > 0 && !digit[k])
+      k--;
+
+		if(!digit[k]) {
+			delete digit;
+
+			size = 0;
+			digit = nullptr;
+		} else {
+			size = k + 1;
+			digit = (digit_t*)realloc(digit, sizeof(digit_t)*size);
+		}
   }
 
   // convert x to base 2^exp
   template <typename T> static bint<exp, digit_t, digit2_t> from(T x) {
+    short sign = 1;
+
+		if(x < 0) {
+			sign = -1;
+			x = -x;
+		}
+
     size_t s = 10;
     digit_t *v = new digit_t[s];
 
@@ -73,104 +111,140 @@ public:
       v[i] = modPow2(x, exp);
     }
 
-    return bint(v, i + 1);
+    return bint(v, i + 1, sign);
   }
 
 
 	// sum the absolute values of the big integers with digits x[0...a]
 	// and y[0...b] and save in z[0...a + 1]. It's assumed that a >= b.
 	// All the memory should be pre-allocated before execution.
-	static short add(digit_t* x, size_t a, digit_t* y, size_t b, digit_t* z) {
+	static void abs_add_digits(bint_t* x, bint_t* y, bint_t* z) {
 		digit_t carry = 0;
 
 		size_t i = 0;
 
+		size_t a = x->size;
+		size_t b = y->size;
+
+		if(a < b) {
+			bint_t* t = x;
+			x = y;
+			y = t;
+
+			size_t c = a;
+			a = b;
+			b = c;
+		}
+
+		z->resize(x->size + 1);
+
 		for(; i < b; ++i) {
-			carry += x[i] + y[i];
-			z[i] = (digit_t)(carry & mask);
+			carry += x->digit[i] + y->digit[i];
+			z->digit[i] = (digit_t)(carry & mask);
 			carry >>= exp;
 		}
 
 		for(; i < a; ++i) {
-			carry += x[i];
-			z[i] = (digit_t)(carry & mask);
+			carry += x->digit[i];
+			z->digit[i] = (digit_t)(carry & mask);
 			carry >>= exp;
 		}
 
-		z[i] = carry;
-		return 1;
+		z->digit[i] = carry;
+
+		return z->trim();
 	}
 
 	// subtract the absolute values of the big integers with digits x[0...a]
 	// and y[0...b] and save in z[0...a]. It's assumed that a >= b.
 	// All the memory should be pre-allocated before execution.
-	static short sub(digit_t* x, size_t a, digit_t* y, size_t b, digit_t* z) {
+	static void abs_sub_digits(bint_t* x, bint_t* y, bint_t* z) {
 		short sign = 1;
+
+		size_t a = x->size;
+		size_t b = y->size;
 
 		size_t i;
 
+		if(b > a) {
+			sign = -1;
 
-		// Get the index of the digit that x and y differ
-		if(a == b) {
+			bint_t* t = x;
+			x = y;
+			y = t;
+
+			size_t c = a;
+			a = b;
+			b = c;
+		} else if(a == b) {
+			// Get the index of the digit that x and y differ
 			i = a;
-			while(--i >= 0 && x[i] == y[i]);
 
-			if(i < 0) {
-				z[0] = 0;
-				return 1;
+			while(i > 0 && x->digit[i] == y->digit[i]) i = i - 1;
+
+			if(i == 0 && x->digit[i] == y->digit[i]) {
+				z->resize(0);
+				z->size = 0;
+			  z->sign = 1;
+				return;
 			}
 
-			if(x[i] < y[i]) {
-				sign = -1;
-
-				digit_t *t = x;
-				size_t   c = a;
-
+			if(x->digit[i] < y->digit[i]) {
+				bint_t* t = x;
 				x = y;
 				y = t;
+
+				size_t c = a;
 				a = b;
 				b = c;
+
+				sign = -1;
 			}
 
-			a = i + 1;
-			b = a;
+			a = b = i + 1;
 		}
 
 		digit_t borrow = 0;
 
+		z->resize(x->size + 1);
+
 		for(i = 0; i < b; ++i) {
-			borrow = x[i] - y[i] - borrow;
-			z[i] = borrow & mask;
+			borrow = x->digit[i] - y->digit[i] - borrow;
+			z->digit[i] = borrow & mask;
 			borrow >>= exp;
 			borrow &= 1;
 		}
 
 		for(; i < a; ++i) {
-			borrow = x[i] - borrow;
-			z[i] = borrow & mask;
+			borrow = x->digit[i] - borrow;
+			z->digit[i] = borrow & mask;
 			borrow >>= exp;
 			borrow &= 1;
 		}
 
-		return sign;
+		z->sign = sign;
+
+		return z->trim();
 	}
 
 	// Square the bint of digits x[0...a] and store the
 	// result in w[0...2*a]
-	static short square(digit_t* x, size_t a, digit_t* w) {
+	static void abs_square_digits(bint_t* x, bint_t* w) {
 		size_t i = 0;
 
-		for(; i < 2*a; i++) {
-			w[i] = 0;
+		w->resize(2*x->size);
+
+		for(; i < 2*x->size; i++) {
+			w->digit[i] = 0;
 		}
 
-		for(i = 0; i < a; i++) {
+		for(i = 0; i < x->size; i++) {
 			digit2_t carry;
-			digit2_t xi = x[i];
+			digit2_t xi = x->digit[i];
 
-			digit_t *pw = w + (i << 1);
-			digit_t *px = x + (i + 1);
-			digit_t *pe = x + a;
+			digit_t *pw = w->digit + (i << 1);
+			digit_t *px = x->digit + (i + 1);
+			digit_t *pe = x->digit + x->size;
 
 			carry = *pw + xi * xi;
 			*pw++ = (digit_t)(carry & mask);
@@ -198,21 +272,25 @@ public:
 			assert((carry >> exp) == 0);
 		}
 
-		return 1;
+		return w->trim();
 	}
 
 	// multiply the big integer with digits in x[0...a] with y[0...b]
 	// and store the result in z[a + b]. It is assumed that a >= b.
 	// All the space should be pre-alocated before execution
-	static short mul(digit_t* x, size_t a, digit_t* y, size_t b, digit_t* z) {
-		for(size_t i = 0; i < a; i++) {
+	static void abs_mul_digits(bint_t* x, bint_t* y, bint_t* z) {
+		z->resize(x->size + y->size);
+
+		if(x->size + y->size == 0) return;
+
+		for(size_t i = 0; i < x->size; i++) {
 			digit2_t carry = 0;
-			digit2_t xi = x[i];
+			digit2_t xi = x->digit[i];
 
-			digit_t *pz = z + i;
-			digit_t *py = y;
+			digit_t *pz = z->digit + i;
+			digit_t *py = y->digit;
 
-			digit_t *pe = y + b;
+			digit_t *pe = y->digit + y->size;
 
 			while(py < pe) {
 				carry += *pz + *py++ * xi;
@@ -228,13 +306,213 @@ public:
 			assert((carry >> exp) == 0);
 		}
 
-		return 1;
+		return z->trim();
 	}
 
-	//TODO: currently, the binary operations only work
-	//      if the size of the first operand is bigger
-	//      or equal than the size of the second, fix
-	//      that by considering adding a sign flag.
+
+	static void add(bint_t* x, bint_t* y, bint_t* z) {
+		bint_t* t = nullptr;
+
+		if(x->sign < 0) {
+			if(y->sign < 0) {
+				z->sign = -1 * z->sign;
+				return abs_add_digits(x, y, z);
+			}
+
+			return abs_sub_digits(y, x, z);
+		}
+
+		if(y->sign < 0) {
+			return abs_sub_digits(x, y, z);
+		}
+
+		return abs_add_digits(x, y, z);
+		/*
+			if(y->size == 0) {
+			z->resize(x->size);
+			z->digit = (digit_t*)memcpy(x->digit, z->digit,
+			sizeof(digit_t)*x->size); z->sign = x->sign; z->size = x->size; return;
+			}
+
+			if(x->size == 0) {
+			z->resize(y->size);
+			z->digit = (digit_t*)memcpy(y->digit, z->digit,
+			sizeof(digit_t)*y->size); z->sign = y->sign; z->size = y->size; return;
+			}
+
+
+			// -x + -y = -z or x + y = z
+			if(x->sign == y->sign) {
+
+			if (y->size > x->size) {
+			t = x;
+			x = y;
+			y = t;
+			}
+
+			z->resize(x->size + 1);
+			z->sign = x->sign;
+
+			abs_add_digits(x->digit, x->size, y->digit, y->size,
+			z->digit);
+
+			return z->trim();
+			}
+
+			// -x + y = z
+			if(x->sign < 0) {
+			if(y->size > x->size) {
+			z->sign = -1;
+			t = x;
+			x = y;
+			y = t;
+			} else {
+			z->sign = 1;
+			}
+
+			x->printRep();
+			y->printRep();
+
+			z->resize(x->size + 1);
+
+			z->sign *= abs_sub_digits(x->digit, x->size, y->digit,
+			y->size, z->digit); z->printRep(); return z->trim();
+			}
+
+			// x + -y
+
+			if(y->size > x->size) {
+			z->sign = -1;
+			t = x;
+			x = y;
+			y = t;
+			} else {
+			z->sign = 1;
+			}
+
+			z->resize(x->size + 1);
+
+			z->sign *= abs_sub_digits(x->digit, x->size, y->digit,
+			y->size, z->digit);
+
+			return z->trim();
+		*/
+	}
+
+	static void sub(bint_t* x, bint_t* y, bint_t * z) {
+		if(x->sign < 0) {
+			if(y->sign < 0) {
+				return abs_sub_digits(y, z, z);
+			}
+			abs_add_digits(x, y, z);
+			if(z->size) {
+				z->sign = -1 * z->sign;
+			}
+		}
+
+		if(y->sign < 0) {
+			return abs_add_digits(x, y, z);
+		}
+
+		return abs_sub_digits(x, y, z);
+		// if(x->size == 0 && y->size == 0) {
+		// 	z->resize(0);
+		// 	return;
+		// }
+
+		// if(y->size == 0) {
+		// 	z->resize(x->size);
+
+		// 	z = memcpy(x->digit, z->digit, sizeof(digit_t)*x->size);
+		// 	z->sign = x->sign;
+		// 	z->size = x->size;
+		// 	return;
+		// }
+
+		// if(x->size == 0) {
+		// 	z->resize(y->size);
+
+		// 	z = memcpy(y->digit, z->digit, sizeof(digit_t)*y->size);
+		// 	z->sign = -y->sign;
+		// 	z->size = y->size;
+		// 	return;
+		// }
+
+		// bint_t* t = nullptr;
+
+		// // a - -b = a + b
+		// if(x->sign > 0 && y->sign < 0) {
+		// 	if(y->size > x->size) {
+		// 		t = x;
+		// 		x = y;
+		// 		y = t;
+		// 	}
+
+		// 	z->resize(x->size + 1);
+
+		// 	z->sign = 1;
+
+		// 	z->sign *= abs_add_digits(x->digit, x->size, y->digit, y->size, z->digit);
+
+		// 	return z->trim();
+		// }
+
+		// // -a - b = -(a + b)
+		// if(x->sign < 0 && y->sign > 0) {
+		// 	if(y->size > x->size) {
+		// 		t = x;
+		// 		x = y;
+		// 		y = t;
+		// 	}
+
+		// 	z->resize(x->size + 1);
+
+		// 	z->sign = 1;
+
+		// 	z->sign *= abs_add_digits(x->digit, x->size, y->digit, y->size, z->digit);
+
+		// 	return z->trim();
+		// }
+
+		// // -a - -b = -a + b
+		// if(x->sign < 0 && y->sign < 0) {
+		// 	if(y->size > x->size) {
+		// 		z->sign = 1;
+		// 		t = x;
+		// 		x = y;
+		// 		y= t;
+		// 	} else if(x->size > y->size) {
+		// 		z->sign = -1;
+		// 	} else {
+		// 		z->sign = 1;
+		// 	}
+
+		// 	z->resize(x->size + 1);
+
+		// 	z->sign *= abs_sub_digits(x->digit, x->size, y->digit, y->size, z->digit);
+
+		// 	return z->trim();
+		// }
+
+		// // a - b
+		// if(y->size > x->size) {
+		// 	z->sign = -1;
+		// 	t = x;
+		// 	x = y;
+		// 	y = t;
+		// } else {
+		// 	z->sign = 1;
+		// }
+
+		// z->resize(x->size + 1);
+
+		// z->sign *= abs_sub_digits(x->digit, x->size, y->digit, y->size, z->digit);
+
+		// return z->trim();
+	}
+
+
+
  	//TODO: fast division for bints of size 1
 	//TODO: long division
 	//TODO: to string of the number in base 10
@@ -246,6 +524,20 @@ public:
 	//TODO: add lcm method
 	//TODO: add construction from strings and const char* types
 
+
+	static short compare(bint_t* v0, bint_t* v1) {
+		if(v0->sign != v1->sign) return v0->sign > v1->sign ? 1 : -1;
+		if(v0->size != v1->size) return v0->size > v1->size ? 1 : -1;
+
+		size_t i = v0->size - 1;
+
+		while(i > 0 && v0->digit[i] == v1->digit[i]) --i;
+
+		if(i == 0 && v0->digit[0] == v1->digit[0]) return 0;
+
+		return v0->digit[i] > v1->digit[i] ? 1 : -1;
+	}
+
 	void print() {
 		for(size_t i = size - 1; i > 0; i--) {
 			digit[i] * pow2(exp);
@@ -253,6 +545,12 @@ public:
 	}
 
   void printRep() {
+		if(size == 0) {
+			std::cout << 0 << std::endl;
+		}
+		if(sign > 0) std::cout << "+";
+		else std::cout << "-";
+
     for (size_t i = size - 1; i > 0; i--)
       std::cout << digit[i] << ".";
     std::cout << digit[0] << std::endl;
--- tests/Core/AST/Int.cpp
+++ tests/Core/AST/Int.cpp
@@ -42,145 +42,185 @@ void should_create_bints_from_types() {
 	assert(v1.digit[0] == 123);
 }
 
-
-void should_add_bints() {
+void should_abs_add_digits_bints() {
 	bint<1> v0 = bint<1>::from<int>(2);
   bint<1> v1 = bint<1>::from<int>(2);
+	bint<1> v2;
 
-	bint<1>::digit_t* a = new bint<1>::digit_t[v0.size + 1];
-	bint<1>::add(v0.digit, v0.size, v1.digit, v1.size, a);
+	bint<1>::abs_add_digits(&v0, &v1, &v2);
 
-	assert(a[0] == 0);
-	assert(a[1] == 0);
-	assert(a[2] == 1);
+	assert(v2.digit[0] == 0);
+	assert(v2.digit[1] == 0);
+	assert(v2.digit[2] == 1);
 
-	bint<3> v2 = bint<3>::from<int>(923456);
-  bint<3> v3 = bint<3>::from<int>(187654);
+	bint<3> v3 = bint<3>::from<int>(923456);
+  bint<3> v4 = bint<3>::from<int>(187654);
+	bint<3> v5;
+	bint<3>::abs_add_digits(&v3, &v4, &v5);
 
-	bint<3>::digit_t *b = new bint<3>::digit_t[v2.size + 1];
-	bint<3>::add(v2.digit, v2.size, v3.digit, v3.size, b);
+	assert(v5.digit[0] == 6);
+	assert(v5.digit[1] == 0);
+	assert(v5.digit[2] == 1);
+	assert(v5.digit[3] == 2);
+	assert(v5.digit[4] == 7);
+	assert(v5.digit[5] == 1);
+	assert(v5.digit[6] == 4);
 
-	assert(b[0] == 6);
-	assert(b[1] == 0);
-	assert(b[2] == 1);
-	assert(b[3] == 2);
-	assert(b[4] == 7);
-	assert(b[5] == 1);
-	assert(b[6] == 4);
-
-	delete[] a;
-	delete[] b;
 }
 
-void should_sub_bints() {
+void should_abs_sub_digits_bints() {
 	bint<1> v0 = bint<1>::from<int>(3);
   bint<1> v1 = bint<1>::from<int>(2);
+	bint<1> v2;
 
-	bint<1>::digit_t* a = new bint<1>::digit_t[v0.size + 1];
-	bint<1>::sub(v0.digit, v0.size, v1.digit, v1.size, a);
+	bint<1>::abs_sub_digits(&v0, &v1, &v2);
 
-	assert(a[0] == 1);
-	assert(a[1] == 0);
-	assert(a[2] == 0);
+	assert(v2.digit[0] == 1);
+	assert(v2.digit[1] == 0);
+	assert(v2.digit[2] == 0);
 
-	bint<3> v2 = bint<3>::from<int>(923456);
-  bint<3> v3 = bint<3>::from<int>(187654);
+	bint<3> v3 = bint<3>::from<int>(923456);
+  bint<3> v4 = bint<3>::from<int>(187654);
+	bint<3> v5;
 
-	bint<3>::digit_t *b = new bint<3>::digit_t[v2.size];
-	bint<3>::sub(v2.digit, v2.size, v3.digit, v3.size, b);
+	bint<3>::abs_sub_digits(&v3, &v4, &v5);
 
-	assert(b[0] == 2);
-	assert(b[1] == 7);
-	assert(b[2] == 0);
-	assert(b[3] == 5);
-	assert(b[4] == 3);
-	assert(b[5] == 6);
-	assert(b[6] == 2);
-
-	delete[] a;
-	delete[] b;
+	assert(v5.digit[0] == 2);
+	assert(v5.digit[1] == 7);
+	assert(v5.digit[2] == 0);
+	assert(v5.digit[3] == 5);
+	assert(v5.digit[4] == 3);
+	assert(v5.digit[5] == 6);
+	assert(v5.digit[6] == 2);
 }
 
-void should_mul_bints() {
+void should_abs_mul_digits_bints() {
 	bint<1> v0 = bint<1>::from<int>(3);
   bint<1> v1 = bint<1>::from<int>(2);
+	bint<1> v2;
 
-	bint<1>::digit_t* a = new bint<1>::digit_t[v0.size + v1.size];
-	bint<1>::mul(v0.digit, v0.size, v1.digit, v1.size, a);
+	bint<1>::abs_mul_digits(&v0, &v1, &v2);
 
-	assert(a[0] == 0);
-	assert(a[1] == 1);
-	assert(a[2] == 1);
-	assert(a[3] == 0);
-	assert(a[4] == 0);
-	assert(a[5] == 0);
+	assert(v2.digit[0] == 0);
+	assert(v2.digit[1] == 1);
+	assert(v2.digit[2] == 1);
+	assert(v2.digit[3] == 0);
+	assert(v2.digit[4] == 0);
+	assert(v2.digit[5] == 0);
 
-	delete[] a;
 
-	bint<3> v2 = bint<3>::from<int>(923456);
-  bint<3> v3 = bint<3>::from<int>(187654);
+	bint<3> v3 = bint<3>::from<int>(923456);
+  bint<3> v4 = bint<3>::from<int>(187654);
+	bint<3> v5;
 
-	bint<3>::digit_t *b = new bint<3>::digit_t[v2.size + v3.size];
-	bint<3>::mul(v2.digit, v2.size, v3.digit, v3.size, b);
+	bint<3>::abs_mul_digits(&v3, &v4, &v5);
 
-	assert(b[0] == 0);
-	assert(b[1] == 0);
-	assert(b[2] == 6);
-	assert(b[3] == 5);
-	assert(b[4] == 4);
-	assert(b[5] == 5);
-	assert(b[6] == 1);
-	assert(b[7] == 7);
-	assert(b[8] == 0);
-	assert(b[9] == 3);
-	assert(b[10] == 1);
-	assert(b[11] == 4);
-	assert(b[12] == 2);
-
-	delete[] b;
+	assert(v5.digit[0] == 0);
+	assert(v5.digit[1] == 0);
+	assert(v5.digit[2] == 6);
+	assert(v5.digit[3] == 5);
+	assert(v5.digit[4] == 4);
+	assert(v5.digit[5] == 5);
+	assert(v5.digit[6] == 1);
+	assert(v5.digit[7] == 7);
+	assert(v5.digit[8] == 0);
+	assert(v5.digit[9] == 3);
+	assert(v5.digit[10] == 1);
+	assert(v5.digit[11] == 4);
+	assert(v5.digit[12] == 2);
 }
 
-void should_sqr_bints() {
+void should_abs_square_digits_bints() {
 
 	bint<1> v0 = bint<1>::from<int>(3);
-	bint<1>::digit_t* a = new bint<1>::digit_t[2*v0.size];
-	bint<1>::square(v0.digit, v0.size, a);
+	bint<1> v1;
 
-	assert(a[0] == 1);
-	assert(a[1] == 0);
-	assert(a[2] == 0);
-	assert(a[3] == 1);
+	bint<1>::abs_square_digits(&v0, &v1);
 
-	delete[] a;
+	assert(v1.digit[0] == 1);
+	assert(v1.digit[1] == 0);
+	assert(v1.digit[2] == 0);
+	assert(v1.digit[3] == 1);
 
 	bint<3> v2 = bint<3>::from<int>(923456);
-	bint<3>::digit_t* b = new bint<3>::digit_t[2*v2.size];
-	bint<3>::square(v2.digit, v2.size, b);
+	bint<3> v3;
 
-	assert(b[0] == 0);
-	assert(b[1] == 0);
-	assert(b[2] == 0);
-	assert(b[3] == 0);
-	assert(b[4] == 1);
-	assert(b[5] == 1);
-	assert(b[6] == 7);
-	assert(b[7] == 0);
-	assert(b[8] == 5);
-	assert(b[9] == 1);
-	assert(b[10] == 2);
-	assert(b[11] == 3);
-	assert(b[12] == 4);
-	assert(b[13] == 1);
+	bint<3>::abs_square_digits(&v2, &v3);
 
-	delete[] b;
+	assert(v3.digit[0] == 0);
+	assert(v3.digit[1] == 0);
+	assert(v3.digit[2] == 0);
+	assert(v3.digit[3] == 0);
+	assert(v3.digit[4] == 1);
+	assert(v3.digit[5] == 1);
+	assert(v3.digit[6] == 7);
+	assert(v3.digit[7] == 0);
+	assert(v3.digit[8] == 5);
+	assert(v3.digit[9] == 1);
+	assert(v3.digit[10] == 2);
+	assert(v3.digit[11] == 3);
+	assert(v3.digit[12] == 4);
+	assert(v3.digit[13] == 1);
+}
+
+void should_add_bints() {
+	bint<3> v0 = bint<3>::from(10);
+	bint<3> v1 = bint<3>::from(10);
+	bint<3> v2;
+
+	bint<3>::add(&v0, &v1, &v2);
+	bint<3> r0 = bint<3>::from(20);
+	assert(bint<3>::compare(&v2, &r0) == 0);
+
+	bint<3> v3 = bint<3>::from(-2);
+	bint<3> v4 = bint<3>::from(-2);
+	bint<3> v5;
+
+	bint<3>::add(&v3, &v4, &v5);
+	bint<3> r1 = bint<3>::from(-4);
+	assert(bint<3>::compare(&v5, &r1) == 0);
+
+	bint<3> v6 = bint<3>::from(5);
+	bint<3> v7 = bint<3>::from(-2);
+	bint<3> v8;
+
+	bint<3>::add(&v6, &v7, &v8);
+	bint<3> r2 = bint<3>::from(3);
+	assert(bint<3>::compare(&v8, &r2) == 0);
+
+	bint<3> v9 = bint<3>::from(-2);
+	bint<3> v10 = bint<3>::from(5);
+	bint<3> v11;
+
+	bint<3>::add(&v9, &v10, &v11);
+	v11.printRep();
+	bint<3> r3 = bint<3>::from(3);
+	assert(bint<3>::compare(&v11, &r3) == 0);
+
+	bint<3> v12 = bint<3>::from(10);
+	bint<3> v13 = bint<3>::from(3);
+	bint<3> v14;
+
+	bint<3>::add(&v12, &v13, &v14);
+	bint<3> r4 = bint<3>::from(13);
+	assert(bint<3>::compare(&v14, &r4) == 0);
+
+	bint<3> v15 = bint<3>::from(3);
+	bint<3> v16 = bint<3>::from(10);
+	bint<3> v17;
+
+	bint<3>::add(&v15, &v16, &v17);
+	bint<3> r5 = bint<3>::from(13);
+	assert(bint<3>::compare(&v17, &r5) == 0);
 }
 
 int main() {
   TEST(should_get_quotient_of_div_by_powers_of_two)
   TEST(should_get_remainder_of_div_by_powers_of_two)
   TEST(should_create_bints_from_types)
+	TEST(should_abs_add_digits_bints)
+	TEST(should_abs_sub_digits_bints)
+	TEST(should_abs_mul_digits_bints)
+	TEST(should_abs_square_digits_bints)
 	TEST(should_add_bints)
-	TEST(should_sub_bints)
-	TEST(should_mul_bints)
-	TEST(should_sqr_bints)
 }
