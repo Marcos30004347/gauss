1b898c5557aaaeb3c7e26f7f9c0bd4e89a969e1e
marcos <marcos30004347@gmail.com>
2021-12-22 00:18:50 -0300

    add galois field methods for polynomial expressions

--- src/Core/GaloisField/GaloisField.hpp
+++ src/Core/GaloisField/GaloisField.hpp
@@ -4,6 +4,8 @@
 #include "Core/Polynomial/Polynomial.hpp"
 #include "Core/Algebra/List.hpp"
 
+// TODO: optimize those functions using reference arguments and move semantics
+
 namespace galoisField {
 
 /**
@@ -17,6 +19,29 @@ namespace galoisField {
  * @return The representation of u(x) over Zp[x]
  */
 //ast::Expr gf(ast::Expr u, ast::Expr x, Int p, bool sym = true);
+
+/**
+ * @brief Compute the representation of u in Z[x] over Zp[x]
+ *
+ * @param u A polynomial in Z[x]
+ * @param p A prime integer x
+ * @param sym true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return The representation of u(x) over Zp[x]
+ */
+ast::Expr gf(ast::Expr u, Int p, bool symmetric);
+
+
+/**
+ * @brief Compute the representation of a polynomial expression in Z[x] over Zp[x]
+ *
+ * @param u A polynomial expression in Z[x]
+ * @param p A prime integer x
+ * @param sym true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return The representation of u(x) over Zp[x]
+ */
+ast::Expr gfPolyExpr(ast::Expr u, Int p, bool symmetric);
 
 
 /**
@@ -32,17 +57,6 @@ namespace galoisField {
 ast::Expr groundGf(ast::Expr u, Int s, bool symmetric);
 
 /**
- * @brief Compute the representation of u in Z[x] over Zp[x]
- *
- * @param u A polynomial in Z[x]
- * @param p A prime integer x
- * @param sym true if Zp[x] is in symmetric representation
- * 						or false otherwise, true by default
- * @return The representation of u(x) over Zp[x]
- */
-ast::Expr gf(ast::Expr u, Int p, bool symmetric);
-
-/**
  * @brief Divide a polynomial a(x) by b(x) over Zp[x].
  *
  * @param a A polynomial in Zp[x]
@@ -54,6 +68,21 @@ ast::Expr gf(ast::Expr u, Int p, bool symmetric);
  * @return A list with que quotient and remainder over Zp[x]
  */
 ast::Expr divPolyGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = true);
+
+/**
+ * @brief Divide a polynomial expression a(x) by b(x) over Zp[x].
+ *
+ * @param a A polynomial expression in Zp[x]
+ * @param b A polynomial expression in Zp[x]
+ * @param x The symbol x
+ * @param p A prime integer
+ * @param sym true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return A list with que quotient and remainder over Zp[x]
+ */
+ast::Expr divPolyExprGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = true);
+
+
 
 /**
  * @brief Compute the remainder of the division of a(x) by b(x) over Zp[x].
@@ -69,6 +98,19 @@ ast::Expr divPolyGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = tru
 ast::Expr remPolyGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = true);
 
 /**
+ * @brief Compute the remainder of the division of a(x) by b(x) over Zp[x].
+ *
+ * @param a A polynomial expression in Zp[x]
+ * @param b A polynomial expression in Zp[x]
+ * @param x The symbol x
+ * @param p A prime integer
+ * @param sym true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return The remainder of a(x) by b(x) over Zp[x]
+ */
+ast::Expr remPolyExprGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = true);
+
+/**
  * @brief Compute the quotient of the division of a(x) by b(x) over Zp[x].
  *
  * @param a A polynomial in Zp[x]
@@ -82,6 +124,19 @@ ast::Expr remPolyGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = tru
 ast::Expr quoPolyGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = true);
 
 /**
+ * @brief Compute the quotient of the division of a(x) by b(x) over Zp[x].
+ *
+ * @param a A polynomial expression in Zp[x]
+ * @param b A polynomial expression in Zp[x]
+ * @param x The symbol x
+ * @param p A prime integer
+ * @param sym true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return The quotient of a(x) by b(x) over Zp[x]
+ */
+ast::Expr quoPolyExprGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = true);
+
+/**
  * @brief Compute the monic form of f(x) over Zp[x]
  *
  * @param f A polynomial in Zp[x]
@@ -92,6 +147,18 @@ ast::Expr quoPolyGf(ast::Expr a, ast::Expr b, ast::Expr x, Int p, bool sym = tru
  * @return A list with the content of f(x) and the monic form of f(x) over Zp[x]
  */
 ast::Expr monicPolyGf(ast::Expr f, ast::Expr x, Int p, bool sym = true);
+
+/**
+ * @brief Compute the monic form of f(x) over Zp[x]
+ *
+ * @param f A polynomial Expression in Zp[x]
+ * @param x The symbol x
+ * @param p A prime integer
+ * @param sym true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return A list with the content of f(x) and the monic form of f(x) over Zp[x]
+ */
+ast::Expr monicPolyExprGf(ast::Expr f, ast::Expr x, Int p, bool sym = true);
 
 /**
  * @brief Computes the gcd of a(x) and b(x) over Zp[x]
@@ -122,6 +189,18 @@ ast::Expr addPolyGf(ast::Expr f, ast::Expr g, ast::Expr x, Int p, bool sym = tru
 
 
 /**
+ * @brief Add two polynomials expressions over Zp
+ *
+ * @param f A polynomial expression in Zp
+ * @param g A polynomial expression in Zp
+ * @param p A prime integer
+ * @param sym  true if Zp is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return The result of f(...) + g(...) over Zp
+ */
+ast::Expr addPolyExprGf(ast::Expr f, ast::Expr g, Int p, bool sym = true);
+
+/**
  * @brief Subtract two polynomials over Zp[x]
  *
  * @param f A polynomial in Zp[x]
@@ -135,6 +214,18 @@ ast::Expr addPolyGf(ast::Expr f, ast::Expr g, ast::Expr x, Int p, bool sym = tru
 ast::Expr subPolyGf(ast::Expr f, ast::Expr g, ast::Expr x, Int p, bool sym = true);
 
 /**
+ * @brief Sub two polynomials expressions over Zp
+ *
+ * @param f A polynomial expression in Zp
+ * @param g A polynomial expression in Zp
+ * @param p A prime integer
+ * @param sym  true if Zp is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return The result of f(...) - g(...) over Zp
+ */
+ast::Expr subPolyExprGf(ast::Expr f, ast::Expr g, Int p, bool sym = true);
+
+/**
  * @brief Multiply two polynomials over Zp[x]
  *
  * @param f A polynomial in Zp[x]
@@ -146,6 +237,18 @@ ast::Expr subPolyGf(ast::Expr f, ast::Expr g, ast::Expr x, Int p, bool sym = tru
  * @return The result of f(x) * g(x) over Zp[x]
  */
 ast::Expr mulPolyGf(ast::Expr f, ast::Expr g, ast::Expr x, Int p, bool sym = true);
+
+/**
+ * @brief Multiply two polynomials expressions over Zp
+ *
+ * @param f A polynomial expression in Zp
+ * @param g A polynomial expression in Zp
+ * @param p A prime integer
+ * @param sym  true if Zp is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return The result of f(...) * g(...) over Zp
+ */
+ast::Expr mulPolyExprGf(ast::Expr f, ast::Expr g, Int p, bool sym = true);
 
 /**
  * @brief Compute f(x)^n in Zp[x]/g(x), that is  f(x)^n mod g(x) over Zp[x]
@@ -163,6 +266,20 @@ ast::Expr powModPolyGf(ast::Expr f, ast::Expr g, ast::Expr x, Int n, Int p, bool
 
 
 /**
+ * @brief Compute f(x)^n in Zp[x]/g(x), that is  f(x)^n mod g(x) over Zp[x]
+ *
+ * @param f A polynomial expression in Zp[x]
+ * @param g A polynomial expression in Zp[x]
+ * @param x The symbol x
+ * @param n An integer
+ * @param p A prime integer
+ * @param sym  true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @returnf(x)^n mod g(x) over Zp[x]
+ */
+ast::Expr powModPolyExprGf(ast::Expr f, ast::Expr g, ast::Expr x, Int n, Int p, bool sym = true);
+
+/**
  * @brief Return a random polynomial over Zp[x]
  *
  * @param d The degree of the polynomial
@@ -173,6 +290,18 @@ ast::Expr powModPolyGf(ast::Expr f, ast::Expr g, ast::Expr x, Int n, Int p, bool
  * @return A random polynomial with degree 'd' over Zp[x]
  */
 ast::Expr randPolyGf(Int d, ast::Expr x, Int p, bool sym = true);
+
+/**
+ * @brief Return a random polynomial expression over Zp[x]
+ *
+ * @param d The degree of the polynomial
+ * @param x The symbol x
+ * @param p A prime integer
+ * @param sym true if Zp[x] is in symmetric representation
+ * 						or false otherwise, true by default
+ * @return A random polynomial with degree 'd' over Zp[x]
+ */
+ast::Expr randPolyExprGf(Int d, ast::Expr x, Int p, bool sym = true);
 
 /**
  * @brief Extended Euclidean Algorithm over Zp[x]
--- tests/Core/GaloisField/GaloisField.cpp
+++ tests/Core/GaloisField/GaloisField.cpp
@@ -1,12 +1,15 @@
 #include "Core/GaloisField/GaloisField.hpp"
 #include "Core/AST/AST.hpp"
 #include "Core/Algebra/Algebra.hpp"
+#include "Core/Algebra/List.hpp"
+#include "Core/Polynomial/Polynomial.hpp"
 #include "test.hpp"
 #include <cstdlib>
 
 using namespace ast;
 using namespace algebra;
 using namespace galoisField;
+using namespace polynomial;
 
 void should_project_poly_galois_field() {
   Expr x = Expr("x");
@@ -24,6 +27,32 @@ void should_project_poly_galois_field() {
          3 * power(x, 4) * power(y, 3) + 2 * power(x, 3) + 2 * x * y + 5 * y);
   assert(gf(g, 6, true) ==
          3 * power(x, 4) * power(y, 3) + 2 * power(x, 3) + 2 * x * y + -1 * y);
+}
+
+void should_project_poly_expr_galois_field() {
+  Expr x = Expr("x");
+  Expr y = Expr("y");
+
+  Expr u = polyExpr(
+      15 * power(x, 4) + 8 * power(x, 3) + 6 * power(x, 2) + 8 * x, list({x}));
+
+  assert(gfPolyExpr(u, 5, false) ==
+         polyExpr(3 * power(x, 3) + power(x, 2) + 3 * x, list({x})));
+  assert(gfPolyExpr(u, 5, true) ==
+         polyExpr(-2 * power(x, 3) + power(x, 2) + -2 * x, list({x})));
+
+  Expr g = polyExpr(15 * power(x, 4) * power(y, 3) + 8 * power(x, 3) +
+                        6 * power(x, 2) * power(y, 2) + 8 * x * y + 17 * y,
+                    list({x, y}));
+
+  assert(gfPolyExpr(g, 6, false) ==
+         polyExpr(3 * power(x, 4) * power(y, 3) + 2 * power(x, 3) + 2 * x * y +
+                      5 * y,
+                  list({x, y})));
+  assert(gfPolyExpr(g, 6, true) ==
+         polyExpr(3 * power(x, 4) * power(y, 3) + 2 * power(x, 3) + 2 * x * y +
+                      -1 * y,
+                  list({x, y})));
 }
 
 void should_ground_project_poly_galois_field() {
@@ -55,6 +84,18 @@ void should_add_poly_galois_field() {
   assert(addPolyGf(u, v, x, 5) == power(x, 2) + power(x, 3) + -1 * power(x, 4));
 }
 
+void should_add_poly_expr_galois_field() {
+  Expr x = Expr("x");
+
+  Expr u = polyExpr(
+      15 * power(x, 4) + 8 * power(x, 3) + 6 * power(x, 2) + 8 * x, list({x}));
+  Expr v = polyExpr(4 * power(x, 4) + 3 * power(x, 3) + 5 * power(x, 2) + 2 * x,
+                    list({x}));
+
+  assert(addPolyExprGf(u, v, 5) ==
+         polyExpr(power(x, 2) + power(x, 3) + -1 * power(x, 4), list({x})));
+}
+
 void should_sub_poly_galois_field() {
   Expr x = Expr("x");
 
@@ -62,6 +103,18 @@ void should_sub_poly_galois_field() {
   Expr v = 4 * power(x, 4) + 3 * power(x, 3) + 5 * power(x, 2) + 2 * x;
 
   assert(subPolyGf(u, v, x, 5) == x + power(x, 2) + power(x, 4));
+}
+
+void should_sub_poly_expr_galois_field() {
+  Expr x = Expr("x");
+
+  Expr u = polyExpr(
+      15 * power(x, 4) + 8 * power(x, 3) + 6 * power(x, 2) + 8 * x, list({x}));
+  Expr v = polyExpr(4 * power(x, 4) + 3 * power(x, 3) + 5 * power(x, 2) + 2 * x,
+                    list({x}));
+
+  assert(subPolyExprGf(u, v, 5) ==
+         polyExpr(x + power(x, 2) + power(x, 4), list({x})));
 }
 
 void should_mul_poly_galois_field() {
@@ -74,6 +127,20 @@ void should_mul_poly_galois_field() {
          power(x, 2) + 2 * power(x, 3) + -2 * power(x, 6) + 2 * power(x, 7));
 }
 
+void should_mul_poly_expr_galois_field() {
+  Expr x = Expr("x");
+
+  Expr u = polyExpr(
+      15 * power(x, 4) + 8 * power(x, 3) + 6 * power(x, 2) + 8 * x, list({x}));
+  Expr v = polyExpr(4 * power(x, 4) + 3 * power(x, 3) + 5 * power(x, 2) + 2 * x,
+                    list({x}));
+
+  assert(mulPolyExprGf(u, v, 5) ==
+         polyExpr(power(x, 2) + 2 * power(x, 3) + -2 * power(x, 6) +
+                      2 * power(x, 7),
+                  list({x})));
+}
+
 void should_div_poly_galois_field() {
   Expr x = Expr("x");
 
@@ -81,6 +148,30 @@ void should_div_poly_galois_field() {
   Expr v = 5 * power(x, 2) + 2 * x;
 
   assert(divPolyGf(u, v, x, 7) == list({-1 * x + 3 * power(x, 2) + 3, 2 * x}));
+}
+
+void should_div_poly_expr_galois_field() {
+  Expr x = Expr("x");
+
+  Expr u = polyExpr(
+      15 * power(x, 4) + 8 * power(x, 3) + 6 * power(x, 2) + 8 * x, list({x}));
+  Expr v = polyExpr(5 * power(x, 2) + 2 * x, list({x}));
+
+  assert(divPolyExprGf(u, v, x, 7) ==
+         list({polyExpr(-1 * x + 3 * power(x, 2) + 3, list({x})),
+               polyExpr(2 * x, list({x}))}));
+
+  Expr g = polyExpr(power(x, 2) + -2 * power(x, 3) + 3 * power(x, 4) +
+                        -1 * power(x, 6) + 2 * power(x, 7) + power(x, 8),
+                    list({x}));
+  Expr k = polyExpr(2 * x + 5 * power(x, 2), list({x}));
+
+  assert(
+      divPolyExprGf(g, k, x, 7) ==
+      list({polyExpr(-2 + 2 * x + power(x, 2) + -1 * power(x, 3) +
+                         -1 * power(x, 4) + 2 * power(x, 5) + 3 * power(x, 6),
+                     list({x})),
+            polyExpr(-3 * power(x, 1), list({x}))}));
 }
 
 void should_pow_mod_poly_galois_field() {
@@ -92,6 +183,16 @@ void should_pow_mod_poly_galois_field() {
   assert(powModPolyGf(u, v, x, 3, 7) == x);
 }
 
+void should_pow_mod_poly_expr_galois_field() {
+  Expr x = Expr("x");
+
+  Expr u = polyExpr(
+      15 * power(x, 4) + 8 * power(x, 3) + 6 * power(x, 2) + 8 * x, list({x}));
+  Expr v = polyExpr(5 * power(x, 2) + 2 * x, list({x}));
+
+  assert(powModPolyExprGf(u, v, x, 3, 7) == polyExpr(x, list({x})));
+}
+
 void should_get_monic_form_galois_field() {
   Expr x = Expr("x");
 
@@ -99,6 +200,18 @@ void should_get_monic_form_galois_field() {
 
   assert(monicPolyGf(u, x, 7) ==
          list({3, -2 * x + -3 * power(x, 2) + power(x, 3) + power(x, 4)}));
+}
+
+void should_get_monic_form_poly_expr_galois_field() {
+  Expr x = Expr("x");
+
+  Expr u = polyExpr(3 * power(x, 4) + 3 * power(x, 3) + -2 * power(x, 2) + x,
+                    list({x}));
+
+  assert(monicPolyExprGf(u, x, 7) ==
+         list({polyExpr(3, list({x})),
+               polyExpr(-2 * x + -3 * power(x, 2) + power(x, 3) + power(x, 4),
+                        list({x}))}));
 }
 
 void should_get_gcd_of_poly_galois_field() {
@@ -126,13 +239,20 @@ void should_perform_extended_euclid_galois_field() {
 int main() {
 
   TEST(should_project_poly_galois_field)
+  TEST(should_project_poly_expr_galois_field)
   TEST(should_ground_project_poly_galois_field)
   TEST(should_add_poly_galois_field)
+  TEST(should_add_poly_expr_galois_field)
   TEST(should_sub_poly_galois_field)
+  TEST(should_sub_poly_expr_galois_field)
   TEST(should_mul_poly_galois_field)
+  TEST(should_mul_poly_expr_galois_field)
   TEST(should_div_poly_galois_field)
+  TEST(should_div_poly_expr_galois_field)
   TEST(should_pow_mod_poly_galois_field)
+  TEST(should_pow_mod_poly_expr_galois_field)
   TEST(should_get_monic_form_galois_field)
+  TEST(should_get_monic_form_poly_expr_galois_field)
   TEST(should_get_gcd_of_poly_galois_field)
   TEST(should_perform_extended_euclid_galois_field)
 
